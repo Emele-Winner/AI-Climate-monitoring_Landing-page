8da9fc95079680ec5d642f728231209fd2e34e96
Emele-Winner <winnerumaemele2007@gmail.com>
2025-08-16 21:15:38 -0700

    ......

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV;
 
 const nextConfig: NextConfig = {
-  basePath: isProd ? '/AI-Climate-monitoring_Landing-page/' : "",
+  basePath: isProd ? '/AI-Climate-monitoring_Landing-page' : "",
   assetPrefix: isProd ? '/AI-Climate-monitoring_Landing-page' : "",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
