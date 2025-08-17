9b3ca0a9b2ba41fa720491f86d65bb9922c739cb
Emele-Winner <winnerumaemele2007@gmail.com>
2025-08-16 20:36:18 -0700

    ..

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  basePath: isProd ? '/<AI-Climate-monitoring_Landing-page>' : '',
-  assetPrefix: isProd ? '/<AI-Climate-monitoring_Landing-page>/' : '',
   images: {
     unoptimized: true, // disables Image Optimization (not supported on GH Pages)
   },
